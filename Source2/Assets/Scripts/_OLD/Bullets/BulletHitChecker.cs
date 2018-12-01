0b403ea383a07dc6c0fce62afcea38542cf58403
shk729 <takeshark729@gmail.com>
2018-12-01 17:14:50 +0300

    lost files

---
+++ Source2/Assets/Scripts/_OLD/Bullets/BulletHitChecker.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletHitChecker : MonoBehaviour 
+{
+	[SerializeField]
+	private ParticleSystem hitEffect;
+
+	[SerializeField]
+	private ParticleSystem monsterDie;
+
+	public float damage = 30f;
+
+
+	void OnTriggerEnter2D(Collider2D target)
+	{
+		
+		if (target.gameObject.tag == "Monster") 
+		{
+			target.GetComponent<MonsterAI>().monsterHealth -= damage;
+			target.GetComponent<MonsterAI> ().agressive = true;
+
+			MonsterAI monster = target.GetComponent<MonsterAI> ();
+			if (monster.monsterHealth <= 0) { monster.Die (); }
+		}
+
+	}
+
+	void OnCollisionEnter2D(Collision2D target)
+	{
+		if (target.gameObject.tag == "Player") {
+			
+		}  else {
+
+			//exlosion animation
+			ParticleSystem hitEffect_clone =  Instantiate(hitEffect, transform.position, transform.rotation); 
+			hitEffect_clone.gameObject.SetActive (true);
+			//GetComponent<AudioSource>().PlayOneShot(hitSound);
+			Destroy (hitEffect_clone, hitEffect.duration);
+			Destroy (this.gameObject, 0f);
+		}
+	}
+
+}
---
+++ Source2/Assets/Scripts/pMove.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pMove : RigidPausable {
+    private GameObject player;
+    static Rigidbody2D playerBody;
+    //Vector2 moveDirection;
+
+    public float speed;
+
+    // Use this for initialization
+    void Start () {
+        playerBody = GetComponent<Rigidbody2D>();
+        player = this.gameObject;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void Awake()
+    {
+        
+        body = playerBody;
+    }
+
+    private void FixedUpdate()
+    {
+        KeyboardMove();
+    }
+
+
+    void KeyboardMove()
+    {
+        
+        float forceScale = 5f;
+
+        float inputX = Input.GetAxis("Horizontal");
+        float inputY = Input.GetAxis("Vertical");
+        Vector2 moveDirection = new Vector2(inputX, inputY);
+        ;
+        playerBody.MovePosition(playerBody.position + (moveDirection * speed) );
+    }
+}
