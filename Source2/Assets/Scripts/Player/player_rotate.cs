de6566f32a01550344217c1658629f84526a798a
Petr Lagun <petr.lagun@gmail.com>
2018-12-01 20:09:53 +0100

    player move

---
+++ Source2/Assets/Scripts/Player/player_rotate.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player_rotate : MonoBehaviour {
+
+
+    public Camera cam;
+
+   	void FixedUpdate () {
+
+        Vector3 temp = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10f);
+        Vector3 currentMousePosition = cam.ScreenToWorldPoint(temp);
+
+
+        if (currentMousePosition.x <= 0) this.transform.localScale = new Vector3(1f, 1, 1);
+        else this.transform.localScale = new Vector3(-1f, 1, 1);
+
+    }
+
+
+}
