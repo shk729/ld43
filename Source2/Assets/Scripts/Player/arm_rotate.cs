5fa149145a217830b3cf8f98a67f6fe69dfd64fb
shk729 <takeshark729@gmail.com>
2018-12-01 22:18:29 +0300

    fixes

--- Source2/Assets/Scripts/Player/arm_rotate.cs
+++ Source2/Assets/Scripts/Player/arm_rotate.cs
@@ -19,18 +19,14 @@ public class arm_rotate : MonoBehaviour
          float moveAngle = 0;
          float rotAngle = Mathf.Atan(mouseDirection.normalized.y/ mouseDirection.normalized.x); 
 
-<<<<<<< HEAD
+
          if (mouseDirection.normalized.x>=0)
              moveAngle = rotAngle * Mathf.Rad2Deg-90;
          else if (mouseDirection.normalized.x<0)
              moveAngle = rotAngle * Mathf.Rad2Deg+90;
-
-        Debug.Log(moveAngle);
-        this.gameObject.transform.rotation = Quaternion.Euler (0, 0, moveAngle-90);
-=======
+        
          moveAngle = rotAngle * Mathf.Rad2Deg;
          this.gameObject.transform.rotation = Quaternion.Euler (0, 0, moveAngle);
->>>>>>> de6566f32a01550344217c1658629f84526a798a
 
     }
 
