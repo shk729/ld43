67f1b9e2385bdf63e7977393b06fe04467a455f4
shk729 <takeshark729@gmail.com>
2018-12-02 12:40:15 +0300

    Sword Attack

---
+++ Source2/Assets/SwordAbility.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwordAbility : MonoBehaviour {
+    public GameObject damageZone;
+    public float cooldown;
+    public float attackTime;
+    private float lastAttack;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        if (!Input.GetMouseButton(1)) return;
+		if (IsReady())
+        {
+            Attack();
+            AttackAnimation();
+        }
+	}
+
+    void Attack()
+    {
+        lastAttack = Time.time;
+        damageZone.SetActive(true);
+    }
+
+    void AttackAnimation() { }
+
+    bool IsReady()
+    {
+        float time = Time.time;
+        if (damageZone.active &&  time > lastAttack + attackTime) damageZone.SetActive(false);
+        return time > lastAttack + cooldown;
+    }
+}
+
---
+++ Source2/Assets/SwordHit.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwordHit : MonoBehaviour {
+    public float damage;
+
+
+	void OnCollisionEnter2D(Collision2D target)
+    {
+        if (target.gameObject.tag == "Monster")
+        {
+            target.gameObject.GetComponent<Monster_stat>().Monster_hit( damage );
+        }
+    }
+}
